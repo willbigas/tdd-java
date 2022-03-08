e0ba5b71a14d6914464a14ce068455c88442d504
William Bigas Mauro <williambmauro@hotmail.com>
2022-03-08 15:36:03 -0300

    Refactoring

--- src/main/java/modelo/Desempenho.java
+++ src/main/java/modelo/Desempenho.java
@@ -1,6 +1,27 @@
 package modelo;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
 
-    BOM, OTIMO, A_DESEJAR
+    BOM {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    },
+    OTIMO {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.2");
+        }
+    },
+    A_DESEJAR {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
 }
--- src/main/java/service/ReajusteService.java
+++ src/main/java/service/ReajusteService.java
@@ -8,20 +8,8 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-
-        if (desempenho.equals(Desempenho.A_DESEJAR)) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho.equals(Desempenho.BOM)) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho.equals(Desempenho.OTIMO)) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
-            funcionario.reajustarSalario(reajuste);
-        }
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+        funcionario.reajustarSalario(reajuste);
     }
 }
