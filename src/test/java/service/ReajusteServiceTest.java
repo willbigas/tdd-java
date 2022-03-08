32069b736bc1e8c4f2469c8c26c3916480bc9c9b
William Bigas Mauro <williambmauro@hotmail.com>
2022-03-08 16:08:46 -0300

    extração de metodo privado

--- src/test/java/service/ReajusteServiceTest.java
+++ src/test/java/service/ReajusteServiceTest.java
@@ -2,6 +2,7 @@ package service;
 
 import modelo.Desempenho;
 import modelo.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -11,26 +12,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ReajusteServiceTest {
 
+    private ReajusteService service;
+    private Funcionario funcionario;
+
+    @BeforeEach
+    public void inicializar() {
+        this.service = new ReajusteService();
+        this.funcionario = new Funcionario("William Bigas Mauro" , LocalDate.now() , new BigDecimal("1000.00"));
+    }
+
     @Test
     public void reajusteDeveriaSerDeTrezPorCentoQuandoDesempenhoForADesejar() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("William Bigas Mauro" , LocalDate.now() , new BigDecimal("1000.00"));
         service.concederReajuste(funcionario , Desempenho.A_DESEJAR);
         assertEquals(new BigDecimal("1030.00") , funcionario.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeQuinzPorCentoQuandoDesempenhoForBom() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("William Bigas Mauro" , LocalDate.now() , new BigDecimal("1000.00"));
         service.concederReajuste(funcionario , Desempenho.BOM);
         assertEquals(new BigDecimal("1150.00") , funcionario.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("William Bigas Mauro" , LocalDate.now() , new BigDecimal("1000.00"));
         service.concederReajuste(funcionario , Desempenho.OTIMO);
         assertEquals(new BigDecimal("1200.00") , funcionario.getSalario());
     }
